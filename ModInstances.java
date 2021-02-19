fb661c9115d466c0cb22df50da7674c30f455308
SpiderGeneral <75297694+SpiderGeneral@users.noreply.github.com>
2021-02-19 14:19:40 +0530

    Create ModInstances.java

---
+++ ModInstances.java
@@ -0,0 +1,15 @@
+// you dont need to make this class if you already have it just add the code in it :)
+
+public class ModInstances{
+
+private static ModTimeChanger modTimeChanger
+
+public static void register(HUDManager api) {
+		modNightTime = new ModNightTime();
+		api.register(modNightTime);
+    }
+    
+ public static ModTimeChanger getModTimeChanger) {
+		return modTimeChanger;
+	}   
+   
